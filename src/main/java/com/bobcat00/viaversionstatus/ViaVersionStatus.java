b6d19544d269c52b407cdb62b12cd098b38ee7b4
Bobcat00 <Bobcat00@users.noreply.github.com>
2019-04-29 08:17:52 -0400

    Show player client version

---
+++ src/main/java/com/bobcat00/viaversionstatus/ViaVersionStatus.java
@@ -0,0 +1,67 @@
+// ViaVersionStatus - Logs players' client versions
+// Copyright 2019 Bobcat00
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package com.bobcat00.viaversionstatus;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import us.myles.ViaVersion.api.Via;
+import us.myles.ViaVersion.api.ViaAPI;
+import us.myles.ViaVersion.api.protocol.ProtocolVersion;
+
+public final class ViaVersionStatus extends JavaPlugin implements Listener {
+    
+    //Listener listeners;
+
+    @Override
+    public void onEnable()
+    {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+ 
+    @Override
+    public void onDisable()
+    {
+    }
+    
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e)
+    {
+        Player player = e.getPlayer();
+        ViaAPI api = Via.getAPI(); // Get the API
+        int version = api.getPlayerVersion(player); // Get the protocol version
+        ProtocolVersion key = ProtocolVersion.getProtocol(version);
+        getLogger().info(player.getName() + " is using version " + key.toString() + ".");
+      
+        for (Player p: Bukkit.getServer().getOnlinePlayers())
+        {
+            if (p.hasPermission("viaversionstatus.notify"))
+            {
+                p.sendMessage(ChatColor.GOLD + "Player " + ChatColor.RED + player.getName() +
+                    ChatColor.GOLD + " is using version " + ChatColor.RED + key.toString() + ChatColor.GOLD + ".");
+            }
+        }
+
+    }
+
+}
