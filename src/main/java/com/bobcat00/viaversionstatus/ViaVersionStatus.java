6daea99f2f76522177bbe31dbe516fc6f47602cd
FlailoftheLord <jeddyh15@gmail.com>
2019-04-29 11:01:12 -0400

    add config options

--- src/main/java/com/bobcat00/viaversionstatus/ViaVersionStatus.java
+++ src/main/java/com/bobcat00/viaversionstatus/ViaVersionStatus.java
@@ -8,11 +8,11 @@
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 package com.bobcat00.viaversionstatus;
 
@@ -20,6 +20,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -29,39 +30,35 @@ import us.myles.ViaVersion.api.ViaAPI;
 import us.myles.ViaVersion.api.protocol.ProtocolVersion;
 
 public final class ViaVersionStatus extends JavaPlugin implements Listener {
-    
-    //Listener listeners;
 
-    @Override
-    public void onEnable()
-    {
-        Bukkit.getPluginManager().registerEvents(this, this);
-    }
- 
-    @Override
-    public void onDisable()
-    {
-    }
-    
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e)
-    {
-        Player player = e.getPlayer();
-        ViaAPI api = Via.getAPI(); // Get the API
-        int version = api.getPlayerVersion(player); // Get the protocol version
-        ProtocolVersion key = ProtocolVersion.getProtocol(version);
-        getLogger().info(player.getName() + " is using version " + key.toString() + ".");
-      
-        for (Player p: Bukkit.getServer().getOnlinePlayers())
-        {
-            if (p.hasPermission("viaversionstatus.notify"))
-            {
-                p.sendMessage(ChatColor.GOLD + "Player " + ChatColor.RED + player.getName() +
-                    ChatColor.GOLD + " is using version " + ChatColor.RED + key.toString() + ChatColor.GOLD + ".");
-            }
-        }
+	// Listener listeners;
 
-    }
+	@Override
+	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(this, this);
+		saveDefaultConfig();
+	}
+
+	@Override
+	public void onDisable() {
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@EventHandler(priority = EventPriority.MONITOR)
+	public void onPlayerJoin(PlayerJoinEvent e) {
+		Player player = e.getPlayer();
+		ViaAPI api = Via.getAPI(); // Get the API
+		int version = api.getPlayerVersion(player); // Get the protocol version
+		ProtocolVersion key = ProtocolVersion.getProtocol(version);
+		getLogger().info(player.getName() + " is using version " + key.toString() + ".");
+		String message = ChatColor.translateAlternateColorCodes('&', this.getConfig().get("Message").toString());
+
+		for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+			if (p.hasPermission("viaversionstatus.notify")) {
+				p.sendMessage(message.replace("{user}", player.getName()).replace("{client_version}", version + ""));
+			}
+		}
+
+	}
 
 }
